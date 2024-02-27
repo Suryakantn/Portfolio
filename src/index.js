649f4f8417a2890280805e482ab20358925ef1cf
Suryakant <nirmalsuryakant@gmail.com>
2024-02-27 23:55:30 +0530

    commented unused

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import NoFound from './NoFound';
+// import NoFound from './NoFound';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const CryptoJS = require('crypto-js');
+// const CryptoJS = require('crypto-js');
 getAlert();
 
 function getAlert() {
